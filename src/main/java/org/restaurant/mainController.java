46281822ac0b10a4b6f97979c3f1b8b5b91e4288
basseyotudor <basseyotudor@outlook.com>
2023-01-09 19:27:28 +0100

    mainController partially partially complete

--- src/main/java/org/restaurant/mainController.java
+++ src/main/java/org/restaurant/mainController.java
@@ -1,8 +1,46 @@
 package org.restaurant;
 
+import com.mysql.jdbc.PreparedStatement;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class mainController {
 
+    @FXML
+    private Button close;
+
+    @FXML
+    private JFXButton loginbtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private JFXPasswordField password;
+
+    @FXML
+    private JFXTextField username;
+
+    // Database tools
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    // login button action
+    public void login () {
+        String sql = "SELECT * FROM admin WHERE username = ? and password = ?";
+        connect = database.connectDB();
+
+    }
+    // closes the program
+    public void close() {
+        System.exit(0);
+    }
 }
