45d79b5bb1cab097bdac56621dbd4158f2838aec
basseyotudor <basseyotudor@outlook.com>
2023-01-09 19:05:26 +0100

    Added database required statements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module org.restaurant {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.jfoenix;
+    requires java.sql;
+    requires mysql.connector.java;
 
 
     opens org.restaurant to javafx.fxml;
