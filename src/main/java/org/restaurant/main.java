fad4630cd089fdd3f252ae6cd28e6c7182ad6352
basseyotudor <basseyotudor@outlook.com>
2023-01-09 17:49:13 +0100

    Login form Main.java finalised

--- src/main/java/org/restaurant/main.java
+++ src/main/java/org/restaurant/main.java
@@ -3,16 +3,44 @@ package org.restaurant;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class main extends Application {
+
+    private double x = 0;
+    private double y = 0;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+        Scene scene = new Scene(root);
+
+        root.setOnMousePressed((MouseEvent event) -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+
+            stage.setOpacity(0.8f);
+        });
+
+        root.setOnMouseReleased((MouseEvent event) -> {
+            stage.setOpacity(1);
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setTitle("Login!");
         stage.setScene(scene);
         stage.show();
     }
