26f15d5c1143c189a4d44918c722b7a16418e500
basseyotudor <basseyotudor@outlook.com>
2023-01-13 13:08:35 +0100

    Upadated the dashboard

--- src/main/java/org/restaurant/dashboardController.java
+++ src/main/java/org/restaurant/dashboardController.java
@@ -1,20 +1,25 @@
 package org.restaurant;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class dashboardController implements Initializable {
 
@@ -131,7 +136,108 @@ public class dashboardController implements Initializable {
     @FXML
     private Label username;
 
+
+    // Available food/drinks
+    private String [] status = {"Available", "Not Available"};
+    private String [] categories = {"Meals", "Drink"};
+
+    public void foodCategories() {
+        List<String> listCategories = new ArrayList<>();
+
+        for(String data:categories) {
+            listCategories.add(data);
+        }
+
+        ObservableList listData = FXCollections.observableArrayList(listCategories);
+        food_type.setItems(listData);
+    }
+
+    public void foodStatus() {
+        List<String> listStatus = new ArrayList<>();
+
+        for(String data:status) {
+            listStatus.add(data);
+        }
+
+        ObservableList listData = FXCollections.observableArrayList(listStatus);
+        food_status.setItems(listData);
+
+    }
+
+    // switch forms
+
+    public void switchForm(ActionEvent event) {
+        if(event.getSource() == dashboard_btn) {
+            dashboard_form.setVisible(true);
+            product_form.setVisible(false);
+            order_form.setVisible(false);
+        } else if(event.getSource() == food_btn) {
+            dashboard_form.setVisible(false);
+            product_form.setVisible(true);
+            order_form.setVisible(false);
+        } else if(event.getSource() == orders_btn) {
+            dashboard_form.setVisible(false);
+            product_form.setVisible(false);
+            order_form.setVisible(true);
+        }
+    }
+
+
+    // Stage coordinates
+    private double x = 0;
+    private double y = 0;
+
     // Assigning behaviour
+
+    public void logout() {
+        try {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Are you sure you want to logout?");
+            Optional<ButtonType> option = alert.showAndWait();
+
+            if(option.get().equals(ButtonType.OK)) {
+
+                // Logout
+                logout.getScene().getWindow().hide();
+                // Link the login form
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
+                Stage stage = new Stage();
+                Scene scene = new Scene(root);
+
+                root.setOnMousePressed((MouseEvent event) -> {
+                    x = event.getSceneX();
+                    y = event.getSceneY();
+                });
+
+                root.setOnMouseDragged((MouseEvent event) -> {
+                    stage.setX(event.getSceneX() - x);
+                    stage.setY(event.getSceneY() - y);
+
+                    stage.setOpacity(0.8f);
+                });
+
+                root.setOnMouseReleased((MouseEvent event) -> {
+                    stage.setOpacity(1);
+                });
+                stage.initStyle(StageStyle.TRANSPARENT);
+
+
+
+
+                stage.setScene(scene);
+                stage.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void displayUsername() {
+        String user = data.username;
+        user = user.substring(0, 1).toUpperCase() + user.substring(1);
+        username.setText(user);
+    }
 
     public void close() {
         System.exit(0);
@@ -144,6 +250,9 @@ public class dashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayUsername();
+        foodStatus();
+        foodCategories();
 
     }
 }
